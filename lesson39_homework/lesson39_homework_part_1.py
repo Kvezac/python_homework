5ebc32b384f40dd80cec8fc29b6b0c287a0502f1
Kvezac <Kvezak@gmail.com>
2023-06-11 17:39:41 +0300

    modified:   lesson39_homework_part_1.py 	modified:   lesson39_homework_part_2.py

--- lesson39_homework/lesson39_homework_part_1.py
+++ lesson39_homework/lesson39_homework_part_1.py
@@ -7,3 +7,72 @@ ProductInventory. Класс CustomerManager должен отвечать за 
 update_product() и get_product(). Разделяя управление клиентами и запасами продуктов на отдельные классы,
 вы гарантируете, что каждый класс несет единую ответственность, делая программу более модульной и простой в
 обслуживании."""
+
+
+class CustomerManager:
+    customers = []
+
+    def add_customer(self, customer):
+        self.customers.append(customer)
+
+    def update_customer(self, customer_id, updated_info):
+        for customer in self.customers:
+            if customer['id'] == customer_id:
+                customer.update(updated_info)
+
+    def get_customer(self, customer_id):
+        for customer in self.customers:
+            if customer['id'] == customer_id:
+                return customer
+
+
+class ProductInventory:
+    products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def update_product(self, product_id, updated_info):
+        for product in self.products:
+            if product['id'] == product_id:
+                product.update(updated_info)
+
+    def get_product(self, product_id):
+        for product in self.products:
+            if product['id'] == product_id:
+                return product
+
+
+if __name__ == '__main__':
+    # Пример использования классов
+    customer_manager = CustomerManager()
+    product_inventory = ProductInventory()
+    customer_manager.add_customer({
+        'id': 1,
+        'name': 'Andrey',
+        'email': 'Andrey@example.com'
+
+    })
+    customer_manager.add_customer({
+        'id': 2,
+        'name': 'Sasha',
+        'email': 'Sasha@example.com'
+
+    })
+    product_inventory.add_product({
+        'id': 1,
+        'name': 'Phone',
+        'price': 1000})
+    product_inventory.add_product({
+        'id': 2,
+        'name': 'TV',
+        'price': 1200})
+    customer_manager.update_customer(1, {'email': 'Andrey@example.com'})
+    customer_manager.update_customer(2, {'email': 'Sasha@example.com'})
+    product_inventory.update_product(1, {'price': 2000})
+    product_inventory.update_product(2, {'price': 1000})
+
+    print(customer_manager.get_customer(1))
+    print(customer_manager.get_customer(2))
+    print(product_inventory.get_product(1))
+    print(product_inventory.get_product(2))
--- lesson39_homework/lesson39_homework_part_2.py
+++ lesson39_homework/lesson39_homework_part_2.py
@@ -1,3 +1,5 @@
+from abc import ABC
+
 """Разработайте систему управления библиотекой Python, позволяющую добавлять новые типы книг без изменения
 существующей кодовой базы. В системе должен быть базовый класс Book, определяющий общие свойства и методы для всех
 типов книг. Подклассы, такие как FictionBook, NonFictionBook и ReferenceBook, должны наследоваться от класса Book и
@@ -6,3 +8,83 @@
 Библиотечная система должна по-прежнему иметь возможность беспрепятственно обрабатывать новый тип книг. Придерживаясь
 принципа открытости/закрытости, система управления библиотекой позволит легко расширять ее без необходимости
 изменения основной кодовой базы, повышая удобство сопровождения и снижая риск появления ошибок."""
+
+
+class Book(ABC):
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_title(self):
+        return self.title
+
+    def get_author(self):
+        return self.author
+
+    def get_year(self):
+        return self.year
+
+
+class FictionBook(Book):
+    def __init__(self, title, author, year, genre):
+        super().__init__(title, author, year)
+        self.genre = genre
+
+    def get_genre(self):
+        return self.genre
+
+
+class NonFictionBook(Book):
+    def __init__(self, title, author, year, topic):
+        super().__init__(title, author, year)
+        self.topic = topic
+
+    def get_topic(self):
+        return self.topic
+
+
+class ReferenceBook(Book):
+    def __init__(self, title, author, year, category):
+        super().__init__(title, author, year)
+        self.topic = category
+
+    @property
+    def category(self):
+        return self.category
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def search_book(self, book):
+        for value in self.books:
+            if value == book:
+                return self.book
+
+    def remove_book(self, book):
+        self.books.remove(book)
+
+    def __str__(self):
+        result = ""
+        for ind, book in enumerate(self.books, 1):
+            result += f"{ind}. type: {book.__class__.__name__} {*[i for i in book.__dict__.values()],}\n"
+        return result
+
+
+if __name__ == '__main__':
+    library = Library()
+
+    fiction_book = FictionBook("The Great Gatsby", "F. Scott Fitzgerald", 1925, "Novel")
+    non_fiction_book = NonFictionBook("The Selfish Gene", "Richard Dawkins", 1976, "Evolutionary biology")
+    reference_book = ReferenceBook("The Oxford English Dictionary", "Various", 1884, "Dictionary")
+
+    library.add_book(fiction_book)
+    library.add_book(non_fiction_book)
+    library.add_book(reference_book)
+    print(library)
+
