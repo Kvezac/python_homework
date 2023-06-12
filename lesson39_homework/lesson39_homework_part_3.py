73ef6fc7d268d168f14fee1c58339f86e80d80c7
Kvezac <Kvezak@gmail.com>
2023-06-13 00:01:34 +0300

    modified:   lesson39_homework_part_3.py 	modified:   lesson39_homework_part_4.py 	modified:   lesson39_homework_part_5.py

--- lesson39_homework/lesson39_homework_part_3.py
+++ lesson39_homework/lesson39_homework_part_3.py
@@ -7,3 +7,69 @@ Circle, Rectangle и Triangle. Класс Shape должен служить ба
 не влияя на её выполнение. Разрабатывая иерархию классов в соответствии с принципом замещения Лискова, вы создаете
 систему, которая является более гибкой, расширяемой и способной единообразно обрабатывать операции, связанные с
 формами."""
+
+import math
+
+
+class Shape:
+    def calculate_area(self):
+        pass
+
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return math.pi * self.radius ** 2
+
+    def calculate_perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+    def calculate_perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def calculate_area(self):
+        s = (self.side1 + self.side2 + self.side3) / 2
+        return math.sqrt(s * (s - self.side1) * (s - self.side2) * (s - self.side3))
+
+    def calculate_perimeter(self):
+        return self.side1 + self.side2 + self.side3
+
+
+if __name__ == '__main__':
+    circle = Circle(5)
+    print("Area of circle:", circle.calculate_area())
+    print("Perimeter of circle:", circle.calculate_perimeter())
+
+    rectangle = Rectangle(3, 4)
+    print("Area of rectangle:", rectangle.calculate_area())
+    print("Perimeter of rectangle:", rectangle.calculate_perimeter())
+
+    triangle = Triangle(3, 4, 5)
+    print("Area of triangle:", triangle.calculate_area())
+    print("Perimeter of triangle:", triangle.calculate_perimeter())
+
+    shapes = [circle, rectangle, triangle]
+    for shape in shapes:
+        print("Area of shape:", shape.calculate_area())
+        print("Perimeter of shape:", shape.calculate_perimeter())
