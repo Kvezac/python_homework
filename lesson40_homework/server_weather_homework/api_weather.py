af4cadb2b2c330e9ff0ac5065c5a22a762341afd
Kvezac <Kvezak@gmail.com>
2023-06-17 10:13:25 +0300

    modified:   lesson40_homework/server_weather_homework/api_weather.py 	modified:   lesson40_homework/server_weather_homework/requirements.txt

--- lesson40_homework/server_weather_homework/api_weather.py
+++ lesson40_homework/server_weather_homework/api_weather.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
+import requests
 import os
 
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
 
-
-
-
+def main(city):
+    load_dotenv()
+    API_KEY = os.getenv('API_KEY')
+    url = fr"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    response = requests.get(url)
+    print(response.json())
 
 
 if __name__ == '__main__':
-    print(API_KEY)
+    main('Moscow')
