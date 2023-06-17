8df438c43900c41e1e52edc59a3c94a01bce6a68
Kvezac <Kvezak@gmail.com>
2023-06-17 16:55:58 +0300

    modified:   lesson40_homework/server_weather_homework/api_weather.py 	modified:   lesson40_homework/server_weather_homework/server_weather.py

--- lesson40_homework/server_weather_homework/api_weather.py
+++ lesson40_homework/server_weather_homework/api_weather.py
@@ -3,13 +3,31 @@ import requests
 import os
 
 
-def main(city):
+def get_request(city):
     load_dotenv()
     API_KEY = os.getenv('API_KEY')
-    url = fr"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    url = fr"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=ru&&appid={API_KEY}"
     response = requests.get(url)
-    print(response.json())
+    result = 'Something Went Wrong'
+    if response.status_code == 200:
+        result = ''
+        weather_data = response.json()
+        city = weather_data['name']
+        temp = weather_data['main']['temp']
+        feels_like = weather_data['main']['feels_like']
+        humidity = weather_data['main']['humidity']
+        pressure = weather_data['main']['pressure']
+        description = weather_data['weather'][0]['description']
+        result += f'City name: {city}\n' \
+                  f'Temperature: {temp}℃\n' \
+                  f'The temperature is felt: {feels_like}℃\n' \
+                  f'Humidity: {humidity}%\n' \
+                  f'Pressure: {pressure}hPa\n' \
+                  f'Description: {description}\n' \
+                  f'+++++++++++++++++++++++++++++++\n'
+
+    return result
 
 
 if __name__ == '__main__':
-    main('Moscow')
+    print(get_request('Moscow'))
--- lesson40_homework/server_weather_homework/server_weather.py
+++ lesson40_homework/server_weather_homework/server_weather.py
@@ -1,3 +1,7 @@
+import socket
+import threading
+from api_weather import get_request
+
 """Измените задание номер 2. Добавьте получение прогноза погоды из внешнего источника
    Для этого воспользуйтесь сайтом https://openweathermap.org.
    Для начала нужно зарегистрироваться на сайте по
@@ -6,3 +10,40 @@
    https://openweathermap.org/current есть подробная документация как работать с API. Теперь после запроса от
    клиента необходимо получать данные о погоде с этого
    источника. Полученный результат возвращать клиенту."""
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = 'localhost'
+port = 4321
+
+server_socket.bind((host, port))
+print(f'The server is running at {host}, port {port}')
+
+server_socket.listen(5)
+
+welcome_message = 'Welcome! You are connected to a weather server\n' \
+                  'Enter a message in the form "City": '
+
+
+def handle_client(client_socket, client_address):
+    print(f'Client connected: {client_address}')
+    client_socket.send(welcome_message.encode())
+
+    while True:
+        client_message = client_socket.recv(1024).decode()
+
+        if not client_message:
+            print('Client disconnected: ', client_address)
+            break
+
+        query = client_message.title()
+        answer = get_request(query)
+        client_socket.send(answer.encode())
+
+    client_socket.close()
+
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    client_thread.start()
