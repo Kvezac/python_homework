0050d4bcb6fd93200a776de6808ced26f8b98ccc
Kvezac <Kvezak@gmail.com>
2023-07-13 06:03:39 +0300

    modified:   ../lesson47_homework/database/fill_tables.py 	modified:   ../lesson47_homework/database/models.py 	modified:   ../lesson47_homework/database/sales.db

--- lesson47_homework/database/fill_tables.py
+++ lesson47_homework/database/fill_tables.py
@@ -26,9 +26,10 @@ for _ in range(5):
 for _ in range(50):
     salesman_id = fake.random_int(min=1, max=20)
     customer_id = fake.random_int(min=1, max=5)
-    product = fake.word()
-    price = fake.random_int(min=10, max=1000)
-    sale = Sales(salesman_id=salesman_id, customer_id=customer_id, product=product, price=price)
+    date = fake.date_between(start_date='-3y', end_date='today')
+    print(date)
+    amount = fake.random_int(min=10, max=10000)
+    sale = Sales(date=date, salesman_id=salesman_id, customer_id=customer_id, amount=amount)
     session.add(sale)
 
 session.commit()
--- lesson47_homework/database/models.py
+++ lesson47_homework/database/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DATE
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
@@ -7,10 +9,11 @@ Base = declarative_base()
 class Sales(Base):
     __tablename__ = 'sales'
     id = Column(Integer, primary_key=True)
+    date = Column(DATE, default=datetime.date.year)
     salesman_id = Column(Integer, ForeignKey('salesmen.id'))
     customer_id = Column(Integer, ForeignKey('customers.id'))
-    product = Column(String)
-    price = Column(Integer)
+    # product = Column(String)
+    amount = Column(Integer)
 
 
 class Salesmen(Base):
